a87e98b226890c480982ca197df5dca4ff233607
Edgar Nova <ragnarok540@gmail.com>
2026-04-11 11:15:19 -0500

    rust part 2

--- Rust/conway.rs
+++ Rust/conway.rs
@@ -1,3 +1,6 @@
+use std::{thread, time};
+use std::time::{Duration};
+
 struct Conway<const HEIGHT: usize, const WIDTH: usize> {
     matrix: [[i8; WIDTH]; HEIGHT],
 }
@@ -11,7 +14,7 @@ impl<const HEIGHT: usize, const WIDTH: usize> Conway<HEIGHT, WIDTH> {
         self.matrix[x.rem_euclid(HEIGHT as i8) as usize][y.rem_euclid(WIDTH as i8) as usize] = state;
     }
 
-    fn count_neighbors(self, x: i8, y: i8) -> i8 {
+    fn count_neighbors(&self, x: i8, y: i8) -> i8 {
         let n = self.query(x + 1, y);
         let ne = self.query(x + 1, y + 1);
         let e = self.query(x, y + 1);
@@ -23,19 +26,64 @@ impl<const HEIGHT: usize, const WIDTH: usize> Conway<HEIGHT, WIDTH> {
         n + ne + e + se + s + sw + w + nw
     }
 
+    fn draw(value: i8) -> char {
+        if value == 1 {'#'} else {'.'}
+    }
+
     fn print(&self) {
         for row in self.matrix {
             for x in row {
-                print!("{x}");
+                let c = Self::draw(x);
+                print!("{c}");
             }
             print!("\n");
         }
     }
+
+    fn game_logic(state: i8, neighbors: i8) -> i8 {
+        if state == 1 {
+            if neighbors < 2 {
+                return 0;
+            }
+            if neighbors > 3 {
+                return 0;
+            }
+            state
+        } else {
+             if neighbors == 3 {
+                return 1;
+            }
+            state
+        }
+    }
+
+    fn step_cell(&self, x: i8, y: i8) -> i8 {
+        let state = self.query(x, y);
+        let neighbors = self.count_neighbors(x, y);
+        let next_state = Self::game_logic(state, neighbors);
+        next_state
+    }
+
+    fn simulate(&mut self) {
+        let mut new_matrix = [[0; WIDTH]; HEIGHT];
+        for x in 0..HEIGHT {
+            for y in 0..WIDTH {
+                new_matrix[x][y] = self.step_cell(x as i8, y as i8);
+            }
+        }
+        self.matrix = new_matrix;
+    }
+
+    fn clear_screen() {
+        print!("\x1B[2J");
+    }
+
 }
 
 fn main() {
     const HEIGHT: usize = 10;
     const WIDTH: usize = 20;
+    const HUNDRED_MILLIS: Duration = time::Duration::from_millis(100);
 
     let mut conway = Conway {
         matrix: [[0; WIDTH]; HEIGHT],
@@ -49,8 +97,13 @@ fn main() {
 
     conway.print();
 
-    // let x = conway.count_neighbors(1, 3);
-    // print!("{x}");
+    while true {
+        Conway::<HEIGHT, WIDTH>::clear_screen();
+        conway.simulate();
+        conway.print();
+        thread::sleep(HUNDRED_MILLIS);
+    }
+
 }
 
 // mkdir -p bin
