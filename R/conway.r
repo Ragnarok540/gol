a4051c9fecdff62cc8a831d95f003973b18c74f4
Edgar Nova <ragnarok540@gmail.com>
2026-06-08 15:50:44 -0500

    r refactor

--- R/conway.r
+++ R/conway.r
@@ -1,26 +1,25 @@
-height <- 10
-width <- 20
-vec <- c(0)
-matrix <- array(vec, c(height, width))
-
-query <- function(m, x, y) {
-    return(m[x %% height + 1, y %% width + 1])
+query <- function(g, x, y) {
+    h <- dim(g)[1]
+    w <- dim(g)[2]
+    return(g[x %% h + 1, y %% w + 1])
 }
 
-assign <- function(m, x, y, state) {
-    m[x %% height + 1, y %% width + 1] <- state
-    return(m)
+assign <- function(g, x, y, state) {
+    h <- dim(g)[1]
+    w <- dim(g)[2]
+    g[x %% h + 1, y %% w + 1] <- state
+    return(g)
 }
 
-count_neighbors <- function(m, x, y) {
-    n <- query(m, x + 1, y)
-    ne <- query(m, x + 1, y + 1)
-    e <- query(m, x, y + 1)
-    se <- query(m, x - 1, y + 1)
-    s <- query(m, x - 1, y)
-    sw <- query(m, x - 1, y - 1)
-    w <- query(m, x, y - 1)
-    nw <- query(m, x + 1, y - 1)
+count_neighbors <- function(g, x, y) {
+    n  <- query(g, x + 1, y    )
+    ne <- query(g, x + 1, y + 1)
+    e  <- query(g, x,     y + 1)
+    se <- query(g, x - 1, y + 1)
+    s  <- query(g, x - 1, y    )
+    sw <- query(g, x - 1, y - 1)
+    w  <- query(g, x    , y - 1)
+    nw <- query(g, x + 1, y - 1)
     neighbor_states <- c(n, ne, e, se, s, sw, w, nw)
     return(sum(neighbor_states))
 }
@@ -33,10 +32,10 @@ draw <- function(value) {
     }
 }
 
-print_matrix <- function(m) {
-    for (r in seq(nrow(m))) {
-        for (c in seq(ncol(m))) {
-            cat(draw(m[r, c]))
+print_grid <- function(g) {
+    for (r in seq(nrow(g))) {
+        for (c in seq(ncol(g))) {
+            cat(draw(g[r, c]))
         }
         cat("\n")
     }
@@ -59,41 +58,48 @@ game_logic <- function(state, neighbors) {
     }
 }
 
-step_cell <- function(m, x, y) {
-    state <- query(m, x, y)
-    neighbors <- count_neighbors(m, x, y)
+step_cell <- function(g, x, y) {
+    state <- query(g, x, y)
+    neighbors <- count_neighbors(g, x, y)
     next_state <- game_logic(state, neighbors)
     return(next_state)
 }
 
-simulate <- function(m) {
+simulate <- function(g) {
+    h <- dim(g)[1]
+    w <- dim(g)[2]
     new_vec <- c(0)
-    new_matrix <- array(new_vec, c(height, width))
-    for (r in seq(nrow(m))) {
-        for (c in seq(ncol(m))) {
-            new_state <- step_cell(m, r, c)
-            new_matrix <- assign(new_matrix, r, c, new_state)
+    new_grid <- array(new_vec, c(h, w))
+    for (r in seq(nrow(g))) {
+        for (c in seq(ncol(g))) {
+            new_state <- step_cell(g, r, c)
+            new_grid <- assign(new_grid, r, c, new_state)
         }
     }
-    return(new_matrix)
+    return(new_grid)
 }
 
 clear_screen <- function() {
     cat("\x1b[2J")
 }
 
-matrix = assign(matrix, 1, 4, 1)
-matrix = assign(matrix, 2, 5, 1)
-matrix = assign(matrix, 3, 3, 1)
-matrix = assign(matrix, 3, 4, 1)
-matrix = assign(matrix, 3, 5, 1)
+height <- 10
+width <- 20
+vec <- c(0)
+grid <- array(vec, c(height, width))
 
-print_matrix(matrix)
+grid = assign(grid, 1, 4, 1)
+grid = assign(grid, 2, 5, 1)
+grid = assign(grid, 3, 3, 1)
+grid = assign(grid, 3, 4, 1)
+grid = assign(grid, 3, 5, 1)
+
+print_grid(grid)
 
 repeat {
     clear_screen()
-    matrix <- simulate(matrix)
-    print_matrix(matrix)
+    grid <- simulate(grid)
+    print_grid(grid)
     Sys.sleep(0.1)
 }
 
--- README.md
+++ README.md
@@ -19,8 +19,8 @@ cellular automaton known as [Conway's Game of Life](https://en.wikipedia.org/wik
 |Language|Version|Site|Note|
 |--------|-------|----|----|
 |Ada|10.5.0|[Site](https://www.adaic.org/)|GNAT Compiler|
-|C|C23|[Site](https://www.c-language.org/)||
 |C++|C++23|[Site](https://isocpp.org/)||
+|C|C23|[Site](https://www.c-language.org/)||
 |Erlang|28|[Site](https://www.erlang.org/)||
 |Go|1.26.2|[Site](https://go.dev/)||
 |Io|20151111|[Site](https://iolanguage.org/)|can't clear screen?|
